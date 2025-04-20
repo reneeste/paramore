7f16bbf9d40bf6e874d5f7c26a58d83d369f2e8f
Renee Steinberg <renee.t.steinberg@gmail.com>
2025-04-20 17:04:31 +0300

    Update README and InfoModal with project details, cleanup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Paramore & Hayley Williams Lyric Scraper
 
-Code for a [Paramore & Hayley Williams lyrics searcher](https://reneeste.github.io/paramore). Originally made by [Shayna Kothari for Taylor Swift](https://github.com/shaynak/taylor-swift).
+Code for a [Paramore & Hayley Williams lyrics searcher](https://reneeste.github.io/paramore). Originally made by [Shayna Kothari for Taylor Swift](https://github.com/shaynak/taylor-swift). Re-coded to use Playwright for scraping, optimized and re-factored scraper for multiple artist functionality, changed some design, added more visual album information.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/constants.js
+++ src/components/constants.js
@@ -4,13 +4,13 @@ export const ArtistName = "paramore & hayley williams";
 export const ModalText =
   'Search for a word or a phrase and see all the places Paramore or Hayley Williams have used it in their music! To search for multiple words or phrases, use a comma between them. Use a * for wildcard search.';
 
-export const UpdateText = 'Last updated: 17/04/2025<br/>First published 09/10/2023';
+export const UpdateText = 'Last updated: 17/04/2025<br/>First published: 09/10/2023';
 
 export const ARTISTS = ["Paramore", "Hayley Williams"]; 
 
 // Ordered list of albums for selector - keep in sync with album map
 export const ARTIST_TO_ALBUMS = {
-  "Paramore": [
+  "paramore": [
     "All We Know Is Falling",
     "Riot!",
     "The B-Sides",
@@ -22,10 +22,8 @@ export const ARTIST_TO_ALBUMS = {
     "This Is Why",
     "Re: This Is Why",
   ],
-  "Hayley Williams": [
+  "hayley": [
     "Petals for Armor",
     "FLOWERS for VASES / descansos",
   ]
 }
-
-export const ALBUMS = {}
